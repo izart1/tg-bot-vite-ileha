ff739a899f11761ea1ff65c496cdb8a39797d69b
Artem Izmailov <temalinho@gmail.com>
2022-10-05 16:11:38 +0300

    init

--- src/ProductList/ProductList.jsx
+++ src/ProductList/ProductList.jsx
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import './ProductList.css';
 import ProductItem from '../ProductItem/ProductItem.jsx';
 import {useTelegram} from '../hooks/useTelegram.js';
+
 
 const products = [
     {
@@ -62,7 +63,30 @@ const getTotalPrice = (items) => {
 
 const ProductList = () => {
     const [addedItems, setAddedItems] = useState([]);
-    const {tg} = useTelegram();
+    const {tg, queryId} = useTelegram();
+
+    const onSendData = useCallback(() => {
+        const data = {
+            products: addedItems,
+            totalPrice: getTotalPrice(addedItems),
+            queryId
+        };
+        fetch('http://localhost:8000', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        });
+    }, []);
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData);
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData);
+        };
+    }, [onSendData]);
+
 
     const onAdd = product => {
         const alreadyAdded = addedItems.find(item => item.id === product.id);
